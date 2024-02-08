fbe746f08874b53cd81362a124d5082a05a78ab4
Devin Checa <devincheca@gmail.com>
2024-02-08 07:05:54 -0800

    added attendie logic

--- index.js
+++ index.js
@@ -21,3 +21,42 @@ const canRsvp = () => new Date('8/1/2024') < new Date();
 
 document.onscroll = () => setTimeout(() => document.getElementById('scrollCarrot').style.display = 'none', 3000);
 
+const addAttendie = () => {
+  const list = document.getElementById('attendeesList');
+  const index = list.children.length;
+
+  const newAttendie = document.createElement('div');
+  newAttendie.className = 'new-attendie';
+  newAttendie.appendChild(getMinusButton(index));
+
+  const inputDiv = document.createElement('div');
+  const input = document.createElement('input');
+  input.required = true;
+  input.className = 'input';
+  input.type = 'text';
+  input.name = `attendie_${index}`;
+  input.placeholder = 'Attendie Name';
+  inputDiv.appendChild(input);
+  newAttendie.appendChild(inputDiv);
+
+  list.prepend(newAttendie);
+};
+
+const getMinusButton = index => {
+  const div = document.createElement('div');
+  const button = document.createElement('button');
+  button.className = 'button is-danger';
+  button.type = 'button';
+  button.innerHTML = '-';
+  button.onclick = () => removeAttendie(index);
+  div.appendChild(button);
+  return div;
+};
+
+const removeAttendie = index => {
+  const list = document.getElementById('attendeesList');
+  Array.from(list.children)
+    .filter((_, i) => i === index)
+    .map(child => list.removeChild(child));
+};
+
