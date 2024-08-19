7ebd48c158caf91b4760d0510aac82d51bd0b11a
Devin Checa <devincheca@gmail.com>
2024-08-18 20:33:14 -0700

    Connected upload logic to backend

--- express-static-host/public/album.js
+++ express-static-host/public/album.js
@@ -6,6 +6,8 @@ const LAMBDA_ACTIONS = {
   'GET_URL': 'GET_URL',
 };
 
+const TableName = 'slg-dsc-wed-images';
+
 const uploadPhotos = async () => {
   const fileInput = document.getElementById('file-input');
 
@@ -13,12 +15,15 @@ const uploadPhotos = async () => {
     return showError('Choose a file to upload');
   }
 
+  const Id = crypto.randomUUID();
   const urlResponse = await fetch(LAMBDA_URL, {
     method: 'POST',
     headers: getHeaders(),
     body: JSON.stringify({
       Action: LAMBDA_ACTIONS.GET_URL,
-      Id: crypto.randomUUID(),
+      TableName,
+      Id,
+      Item: { Id },
     }),
   });
 
@@ -59,6 +64,7 @@ const uploadFile = async (file) => {
 }
 
 const onSuccess = () => {
+  // refresh the uploaded image scroller at this point
   console.log('successful upload');
 };
 
@@ -78,97 +84,3 @@ const getHeaders = () => {
   };
 }
 
-/*
-  public async uploadAndVerify() {
-    const [{ signedUrl: faceUrl }, { signedUrl: documentUrl }] = await Promise.all([
-      this.getFaceUploadUrl(),
-      this.getDocumentUploadUrl(),
-    ]);
-
-    this.faceUrl = faceUrl;
-    this.documentUrl = documentUrl;
-
-    if (!this.faceUrl || !this.documentUrl) return;
-
-    await this.uploadFiles();
-  }
-
-  private async uploadFiles() {
-    const faceInputFiles = this.faceInput?.files;
-    const docInputFiles = this.docInput?.files;
-
-    if (
-      faceInputFiles
-      && docInputFiles 
-      && faceInputFiles.length === 1
-      && docInputFiles.length === 1
-      && this.faceInput 
-      && this.docInput
-    ) {
-      this.faceImageType = faceInputFiles[0].type;
-      this.docImageType = faceInputFiles[0].type;
-
-      const faceReader = new FileReader();
-      faceReader.onload = event => event && event.target && this.uploadFile({
-        image: event.target.result,
-        url: this.faceUrl,
-        type: this.faceImageType,
-      });
-      const docReader = new FileReader();
-      docReader.onload = event => event && event.target && this.uploadFile({
-        image: event.target.result,
-        url: this.documentUrl,
-        type: this.docImageType,
-      });
-
-      faceReader.readAsDataURL(faceInputFiles[0]);
-      docReader.readAsDataURL(docInputFiles[0]);
-    }
-  }
-
-  private async uploadFile(file: {
-    image: string | ArrayBuffer | null,
-    url: string,
-    type: string
-  }) {
-    const { image, url, type } = file;
-    if (image && typeof image === 'string') {
-      const binary = atob(image.split(',')[1]);
-      const array = [];
-      for (var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i))
-      }
-      const blobData = new Blob([new Uint8Array(array)], { type })
-      await fetch(url, {
-        method: 'PUT',
-        body: blobData
-      });
-      if (this.isFirstImageUploaded) this.validateId();
-      else this.isFirstImageUploaded = true;
-    }
-  }
-
-  private async validateId() {
-    const response = await fetch(`${LAMBDA.UPLOAD}`, {
-      method: 'POST',
-      headers: this.getHeaders(),
-      body: JSON.stringify({
-        Action: LAMBDA_POST_ACTIONS.VALIDATE_ID,
-        Id: this.id,
-      }),
-    });
-
-    const validation = await response.json();
-    const isSuccess = validation.FaceMatches && validation.FaceMatches
-      .reduce((acc: boolean, { Similarity }: any) => {
-        return acc
-          ? true
-          : Similarity > 95;
-      }, false);
-    const isError = validation.$metadata && validation.$metadata.httpStatusCode !== 200;
-
-    this.onComplete({ validation, isSuccess, isError });
-  }
-
-}
-*/
