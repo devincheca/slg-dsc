a8aae503266b920d69c287c5d7ace29437d970d5
Devin Checa <devincheca@gmail.com>
2025-11-04 18:47:47 -0800

    First attempt to address browser preload

--- next-album/app/page.tsx
+++ next-album/app/page.tsx
@@ -56,6 +56,8 @@ export default function Home() {
             alt={`Gallery Image ${index + 1}`}
             width={300}
             height={200}
+            placeholder="blur"
+            blurDataURL="../public/flowerPng.png"
           />
         ))}
       </div>
